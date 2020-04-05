9afefee1db6206ba66be82239df450ac233c2142
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-05 00:33:38 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,8 +1,15 @@
 class Backer
+  attr_accessor
   attr_reader :backed_projects
+  @@all = []
   
   def initialize(name)
     @name = name
     @backed_projects = []
+    save
+  end
+  
+  def save
+    @@all << self
   end 
 end 
