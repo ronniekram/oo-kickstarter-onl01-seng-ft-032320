35ff9c0cc3375fa7bf59cd0dc4394f803f69c373
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-05 00:44:25 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,20 @@
+class Backer
+  attr_reader :name, :backed_projects
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+    save
+  end
+  
+  def save
+    @@all << self
+  end 
+  
+  def back_project(project)
+    @backed_projects << project
+    project.backers << self
+  end 
+end 
---
+++ lib/project.rb
@@ -0,0 +1,20 @@
+class Project
+  attr_reader :title, :backers
+  
+  @@all = []
+  
+  def initialize(title)
+    @title = title 
+    @backers = []
+    save
+  end 
+  
+  def save 
+    @@all << self
+  end 
+  
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self
+  end 
+end 
